f6adacc0510eb30fe20e20d70cb483c9b3056a69
Jeremy <jeremy.m.legere@gmail.com>
2019-05-03 19:12:34 -0300

    need to add walljumping

--- Assets/Scripts/Controllers/Character/CharacterController2D.cs
+++ Assets/Scripts/Controllers/Character/CharacterController2D.cs
@@ -5,14 +5,14 @@ public class CharacterController2D : MonoBehaviour
 {
 	[SerializeField] private float m_JumpForce = 40f;							// Amount of force added when the player jumps.
 	[Range(0, 1)] [SerializeField] private float m_CrouchSpeed = .36f;			// Amount of maxSpeed applied to crouching movement. 1 = 100%
-	[Range(0, 5000f)] [SerializeField] private float m_MovementSmoothing = 5000f;	// How much to smooth out the movement
-	[SerializeField] private bool m_AirControl = true;							// Whether or not a player can steer while jumping;
+	[Range(0, 2f)] [SerializeField] private float m_MovementSmoothing = 1f;	// How much to smooth out the movement
+	[SerializeField] public bool m_AirControl = true;							// Whether or not a player can steer while jumping;
 	[SerializeField] private LayerMask m_WhatIsGround;							// A mask determining what is ground to the character
 	[SerializeField] private Transform m_GroundCheck;							// A position marking where to check if the player is grounded.
 	[SerializeField] private Transform m_CeilingCheck;							// A position marking where to check for ceilings
 	[SerializeField] private Collider2D m_CrouchDisableCollider;				// A collider that will be disabled when crouching
 
-	const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
+	const float k_GroundedRadius = .05f; // Radius of the overlap circle to determine if grounded
 	private bool m_Grounded;            // Whether or not the player is grounded.
 	const float k_CeilingRadius = .2f; // Radius of the overlap circle to determine if the player can stand up
 	private Rigidbody2D m_Rigidbody2D;
